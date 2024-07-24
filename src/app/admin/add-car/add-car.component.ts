8524a2b2138743e8b5e2e98b5b0f5c040a8d9556
Jeel Khant <141222261+jeelkhant3333@users.noreply.github.com>
2024-07-25 03:01:37 +0530

    Add form validation & css update to global

--- src/app/admin/add-car/add-car.component.ts
+++ src/app/admin/add-car/add-car.component.ts
@@ -1,37 +1,98 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { log } from 'console';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-car',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,],
   templateUrl: './add-car.component.html',
   styleUrl: './add-car.component.css'
 })
 export class AddCarComponent {
 
-  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
-  imageSrc: (string | ArrayBuffer)[] = [];
+  constructor(private formBuilder: FormBuilder) { }
+
+  carData: any = {
+    bodyType: '',
+    color: '',
+    company: '',
+    description: '',
+    fuelType: '',
+    mileage: null,
+    model: '',
+    name: '',
+    price: null,
+    topSpeed: null,
+    transmission: '',
+    images: ['']
+  };
+
+  isInValid: boolean = false
+
+  addCarForm: FormGroup = this.formBuilder.group(
+    {
+      img: [[''], Validators.required],
+      company: ['', Validators.required],
+      name: ['', Validators.required],
+      color: ['', Validators.required],
+      bodyType: ['', Validators.required],
+      trnsmition: ['', Validators.required],
+      model: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', [Validators.required,Validators.min(0)]],
+      fuelType: ['', Validators.required],
+      mileage: ['', [Validators.required,Validators.min(1)]],
+      topSpeed: ['', [Validators.required, Validators.min(1)]],
+    }
+  )
 
 
-  onFileSelected(event: Event) {
-    const files = (event.target as HTMLInputElement).files;
 
-    if (files && files.length > 0) {
-      for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          const result = e.target?.result;
-          if (result) {
-            this.imageSrc = [...this.imageSrc, result as string | ArrayBuffer];
-          }
-        };
-        reader.readAsDataURL(file);
-      }
+  submitForm(): void {
+    if (this.addCarForm.valid) {
+      console.log(this.addCarForm.value);
+    } else {
+      this.isInValid = true
+
     }
   }
+
+  resetForm(): void {
+    this.addCarForm.reset();
+    this.addCarForm.controls['img'].setValue([""]);
+    this.isInValid = false;
+    this.imageSrcs = [];
+  }
+
+
+
+
+
+
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+  imageSrcs: (string | ArrayBuffer)[] = [];
+
+
+  onFileSelected(event: any): void {
+    const files = event.target.files;
+    if (files.length === 0) return;
+
+    const file = files[0];
+    const reader = new FileReader();
+    
+    reader.onload = (e: any) => {
+      this.imageSrcs.push(e.target.result);
+      this.addCarForm.controls['img'].setValue(JSON.stringify(this.imageSrcs));
+      console.log("helloooooooo", this.addCarForm.controls['img']);
+    };
+
+    reader.readAsDataURL(file);
+  }
+
 
 
 }
